d89af1944444847f5784ed0e022e70d4d825a825
m-wheeler0 <sc23mw@leeds.ac.uk>
2026-03-09 08:29:29 +0000

    Implemented authentication for write requests

--- weather_api/weather_api/urls.py
+++ weather_api/weather_api/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("weather.urls"))
+    path("api/", include("weather.urls")),
+    path("api/auth/token/", obtain_auth_token)
 ]
